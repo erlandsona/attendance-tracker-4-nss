d755d640a87bc5ad57225e07251b9ea251ffb8ca
Austin Erlandson <austin@erlandson.com>
2015-06-04 15:45:53 -0500

    Student model tested and implemented.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,10 @@
 class Student < ActiveRecord::Base
+
+  authenticates_with_sorcery!
+
+  validates :name,
+            length: { minimum: 1 },
+            uniqueness: true,
+            presence: true
+
 end
